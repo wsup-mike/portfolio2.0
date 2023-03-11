26b800ba665eb062f735fa0764e0c62a28c17f89
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-11 17:04:22 -0600

    index-style parent div

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen'>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll'>
       <Head>
         <title>Mike's Portfolio Site</title>
       </Head>
