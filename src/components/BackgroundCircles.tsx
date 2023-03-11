c1200b1ecd64b750a3d7efa354dff55b0217d6da
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-11 17:17:17 -0600

    BackgroundCircles-Sub motion.div

--- src/components/BackgroundCircles.tsx
+++ src/components/BackgroundCircles.tsx
@@ -6,13 +6,13 @@ type Props = {}
 
 function BackgroundCircles({}: Props) {
   return (
-    <div className='relative flex justify-center items-center'>
+    <motion.div className='relative flex justify-center items-center'>
         <div className='absolute border border-gray-200 w-[200px] h-[200px] rounded-full mt-52 animate-ping' />
         <div className='absolute border border-gray-200 w-[300px] h-[300px] rounded-full mt-52 animate-ping ' />
         <div className='absolute border border-gray-200 w-[500px] h-[500px] rounded-full mt-52 animate-ping ' />
         <div className='absolute border opacity-20 border-blue-500 w-[650px] h-[650px] rounded-full mt-52 animate-ping ' />
         <div className='absolute border opacity-20 border-blue-500 w-[800px] h-[800px] rounded-full mt-52 animate-ping ' />
-    </div>
+    </motion.div>
   )
 }
 
