8a1aeee1a700d9b9c49805cf9bf9ca460398f0ed
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-11 12:22:06 -0600

    <header> styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 function Header({}: Props) {
   return (
-    <header className='sticky top-0 flex items-start justify-between max-w-7xl mx-auto'>
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
         <div className='flex flex-row item-center'>
             {/* Social Icons */}
             <SocialIcon url='https://www.youtube.com/sonnysangha' fgColor='gray' bgColor='transparent'/>
