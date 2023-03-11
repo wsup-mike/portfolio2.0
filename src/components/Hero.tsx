2c960cedad2b8b511fb46d3b49f20a4991d973bd
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-11 13:28:49 -0600

    Add useTypewriter hook

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,9 +4,21 @@ import { Cursor, useTypewriter } from 'react-simple-typewriter'
 type Props = {}
 
 function Hero({}: Props) {
-  return (
-    <div>Hero</div>
-  )
+    const [text, count] = useTypewriter({
+        words: [
+            "Wsup, Im Mike", 
+            "Adventurer-who-loves-@SushiStation.tsx", 
+            "<LoveFrenchFries />", 
+            "Global-Citizen-Generosity-minded"
+        ],
+        loop: true,
+        delaySpeed: 2000,
+    })
+
+
+    return (
+        <div>Hero</div>
+    )
 }
 
 export default Hero
