ae6849f94e14f3a2e5f6879c9338873cf5ff90c2
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-11 10:31:34 -0600

    Update tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/**/*.{html,js}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
