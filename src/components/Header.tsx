3c88c0b84df0d370e187184bad885f26f33bbd62
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-11 12:31:41 -0600

    Header-import {motion}- n cr8   2 <motion.divs>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion'
 
 type Props = {}
 
 function Header({}: Props) {
   return (
     <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
-        <div className='flex flex-row item-center'>
+        <motion.div className='flex flex-row item-center'>
             {/* Social Icons */}
             <SocialIcon url='https://www.youtube.com/sonnysangha' fgColor='gray' bgColor='transparent'/>
             <SocialIcon url='https://www.youtube.com/sonnysangha' fgColor='gray' bgColor='transparent'/>
             <SocialIcon url='https://www.youtube.com/sonnysangha' fgColor='gray' bgColor='transparent'/>
             <SocialIcon url='https://www.youtube.com/sonnysangha' fgColor='gray' bgColor='transparent'/>
             <SocialIcon url='https://www.youtube.com/sonnysangha' fgColor='gray' bgColor='transparent'/>
-        </div>
-        <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+        </motion.div>
+        <motion.div className='flex flex-row items-center text-gray-300 cursor-pointer'>
             <SocialIcon 
                 className='cursor-pointer'
                 network='email'
@@ -22,7 +23,7 @@ function Header({}: Props) {
                 bgColor='transparent'
             />
             <p className='uppercase hidden md:inline-flex text-xs text-gray-500'>Get in touch</p>
-        </div>
+        </motion.div>
     </header>
 
   )
