a7f95c9cdb822a34d3445da42e777d40dd2a3d11
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-11 10:31:58 -0600

    Test commit index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import type {NextPage} from 'next';
 
 export default function Home() {
   return (
@@ -11,7 +7,7 @@ export default function Home() {
       <Head>
         <title>Mike's Portfolio Site</title>
       </Head>
-      <h1 className='text-red-500'>Hey Sup Ya'll!</h1>
+      <h1 className='text-red-500'>Hey Sup YOu!</h1>
     </div>
   )
 }
