b26604f5389c7ce41479c5a9ad9c4a1835d30cb9
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-11 17:29:22 -0600

    iBackgroundCircles-style motion.div initial,animat

--- src/components/BackgroundCircles.tsx
+++ src/components/BackgroundCircles.tsx
@@ -6,7 +6,20 @@ type Props = {}
 
 function BackgroundCircles({}: Props) {
   return (
-    <motion.div className='relative flex justify-center items-center'>
+    <motion.div 
+        className='relative flex justify-center items-center'
+        initial={{
+            opacity: 0,
+        }}
+        animate={{
+            opacity: [0.1, 0.2, 0.4, 0.8, 1.0],
+            scale: [1,2,2,3,1],
+            borderRadius: ["20%", "20%", "50%", "80%", "20%"]
+        }}
+        transition={{
+            duration: 2.5,
+        }}
+    >
         <div className='absolute border border-gray-200 w-[200px] h-[200px] rounded-full mt-52 animate-ping' />
         <div className='absolute border border-gray-200 w-[300px] h-[300px] rounded-full mt-52 animate-ping ' />
         <div className='absolute border border-gray-200 w-[500px] h-[500px] rounded-full mt-52 animate-ping ' />
