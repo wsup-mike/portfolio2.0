c5e49627b7bc7f8cfb2a0f8ddfd6668b7c80d98e
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-11 16:28:56 -0600

    BackgroundCircles-build test

--- src/components/BackgroundCircles.tsx
+++ src/components/BackgroundCircles.tsx
@@ -7,8 +7,8 @@ type Props = {}
 function BackgroundCircles({}: Props) {
   return (
     <div>
-        <div className='border border-red-500 w-[200px] h-[200px] rounded-full' />
-        <div className='border border-blue-400 h-[180px] w-[180px]' />
+        <div className='border border-red-500 w-[200px] h-[200px] rounded-full mt-52 animate-spin' />
+        <div className='border border-blue-400 ' />
         <div />
         <div />
         <div />
