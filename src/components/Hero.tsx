14e3bfa7e4c2dd144f77462bb6ffc1ea4d29f63f
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-11 17:00:49 -0600

    Hero-style the div

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,11 +18,11 @@ function Hero({}: Props) {
 
 
     return (
-        <div>
+        <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
             <BackgroundCircles />
             <h1>
-            <span>{text}</span>
-            <Cursor cursorColor='#f7ab0a'/>
+                <span>{text}</span>
+                <Cursor cursorColor='#f7ab0a'/>
             </h1>
         </div>
     )
