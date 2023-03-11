a9fa31a693ef1ec7b65d352708ab7e389b561702
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-11 16:43:22 -0600

    BackgroundCircles-build test

--- src/components/BackgroundCircles.tsx
+++ src/components/BackgroundCircles.tsx
@@ -7,9 +7,11 @@ type Props = {}
 function BackgroundCircles({}: Props) {
   return (
     <div className='relative flex justify-center items-center'>
-        <div className='absolute border border-gray-200 border-t-4 w-[200px] h-[200px] rounded-full mt-52 animate-ping' />
-        <div className='absolute border border-gray-200 border-t-4 w-[300px] h-[300px] rounded-full mt-52 animate-ping' />
-        <div className='absolute border border-gray-200 border-t-4 w-[500px] h-[500px] rounded-full mt-52 animate-ping' />
+        <div className='absolute border border-gray-200 w-[200px] h-[200px] rounded-full mt-52 animate-ping' />
+        <div className='absolute border border-gray-200 w-[300px] h-[300px] rounded-full mt-52 animate-ping' />
+        <div className='absolute border border-gray-200 w-[500px] h-[500px] rounded-full mt-52 animate-ping' />
+        <div className='absolute border border-blue-500 w-[650px] h-[650px] rounded-full mt-52 animate-pulse' />
+        <div className='absolute border border-blue-500 w-[800px] h-[800px] rounded-full mt-52 animate-pulse' />
         
         <div />
         <div />
