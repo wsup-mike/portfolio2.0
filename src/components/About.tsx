d910dc75ed8543a1c6737f989f56738a879dd840
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-13 20:57:09 -0500

    Add src of cortez.jpg

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,6 +7,12 @@ function About({}: Props) {
   return (
     <div className='h-screen flex-col relative flex text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
+        <motion.img 
+            src='./assets/cortez.JPG'
+            initial={{
+                
+            }}
+        />
     </div> // now snap to center will snap this component 2 center
   )
 }
