0ef334a482f1bb684cbbb1acb45c53ef4274a808
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 14:26:08 -0500

    Add to motion.div: initial,whileInView,transition

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -5,7 +5,13 @@ type Props = {}
 
 function WorkExperience({}: Props) {
   return (
-    <motion.div className='h-screen flex relative overflow-hidden flex-col md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
+    <motion.div 
+        className='h-screen flex relative overflow-hidden flex-col md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1}}
+        transition={{ duration: 1.0 }}
+    
+    >
         <h3
             className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-2xl'
         >Work Experience</h3>
