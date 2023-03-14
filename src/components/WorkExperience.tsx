fd85b2686dfdf380f9afe4079d8015c4868307f9
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 14:09:40 -0500

    outer div classname atts

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 function WorkExperience({}: Props) {
   return (
-    <div className='h-screen'>
+    <div className='h-screen flex relative overflow-hidden flex-col md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
         <h3
             className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-2xl'
         >Work Experience</h3>
