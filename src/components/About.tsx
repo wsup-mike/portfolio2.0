9b08379f83c949eba856a54cc4b48ae3775f0b3d
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-13 21:13:55 -0500

    style cortex picture

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,14 +9,16 @@ function About({}: Props) {
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
         <motion.img 
             src='./assets/cortez.JPG'
+            className='w-60 h-45 rounded-full'
             initial={{
                 x: -200,
-                opacity: 0,
+                // opacity: 0,
             }}
-            animate={{
-                x: 0,
-                opacity: 1,
-            }}
+            whileInView={{x: 0}}
+            // animate={{
+            //     x: 0,
+            //     opacity: 1,
+            // }}
         />
     </div> // now snap to center will snap this component 2 center
   )
