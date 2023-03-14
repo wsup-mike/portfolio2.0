12049d91a6c6e4952b77b4f676f7f8602b0250e2
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 13:09:22 -0500

    Add 2 motion.div:initial,whileinView,transition

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,12 @@ type Props = {}
 
 function About({}: Props) {
   return (
-    <motion.div className='h-screen flex-col relative flex text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+    <motion.div 
+        className='h-screen flex-col relative flex text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.0 }}
+    >
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
         <motion.img 
             src='./assets/cortez.JPG'
