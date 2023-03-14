1d57f552c2573ab86119082765a69f09d89afc61
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 14:58:54 -0500

    import {motion} and create img as motion.img

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 type Props = {}
 
 function ExperienceCard({}: Props) {
   return (
     <article>
-        <img 
+        <motion.img 
             className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
             src='/assets/netflix.jpg'
         />
