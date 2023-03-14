bf7c1dc064dd0e12245a0dc6ce7ecd460eba6ed4
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-13 20:45:26 -0500

    About-import framer motion {motion}

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 type Props = {}
 
 function About({}: Props) {
   return (
     <div className='h-screen flex-col relative flex text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-        <h3 className='absolute top-24 uppercase tracking-[20px] '>About</h3>
+        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
     </div> // now snap to center will snap this component 2 center
   )
 }
