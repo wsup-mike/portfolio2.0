36bc66e805d7fbe9761fea26723807d63990d338
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 15:03:25 -0500

    2 motion.img:initial,whileInView,viewport,transiti

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -9,6 +9,16 @@ function ExperienceCard({}: Props) {
         <motion.img 
             className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
             src='/assets/netflix.jpg'
+            initial={{
+                y: -100,
+                opacity: 0,
+            }}
+            whileInView={{ 
+                y: 0,
+                opacity: 1,
+            }}
+            viewport={{ once: true }}
+            transition={{ duration: 1.0 }}
         />
     </article>
   )
