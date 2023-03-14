55a05c13c2742eeac20f8d8aa92d42888f06f41e
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 11:43:14 -0500

    New <div> for new <Skill /> component underneat

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -11,6 +11,10 @@ function Skills({}: Props) {
             className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-2xl'
         >Skills</h3>
         <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>Hover for more info</h3>
+
+        <div>
+            
+        </div>
     </motion.div>
   )
 }
