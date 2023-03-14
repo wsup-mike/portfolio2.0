29374a041defec389690509c58f5042ae683c161
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 13:05:04 -0500

    About-wrap outer <div> as motion.div

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 function About({}: Props) {
   return (
-    <div className='h-screen flex-col relative flex text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+    <motion.div className='h-screen flex-col relative flex text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
         <motion.img 
             src='./assets/cortez.JPG'
@@ -28,7 +28,7 @@ function About({}: Props) {
                 Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage prosciutto chicken spare ribs salami picanha doner. Kevin capicola sausage, buffalo bresaola venison turkey shoulder picanha ham pork tri-tip meatball meatloaf ribeye. Doner spare ribs andouille bacon sausage. Ground round jerky brisket pastrami shank.
             </p>
         </div>
-    </div> // now snap to center will snap this component 2 center
+    </motion.div> // now snap to center will snap this component 2 center
   )
 }
 
