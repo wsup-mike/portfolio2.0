1887dfccd22a6e50f0b6838657a5bfffb2bc1f2e
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 14:56:36 -0500

    Add netflix img with atts and src

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -4,7 +4,12 @@ type Props = {}
 
 function ExperienceCard({}: Props) {
   return (
-    <article></article>
+    <article>
+        <img 
+            className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
+            src='/assets/netflix.jpg'
+        />
+    </article>
   )
 }
 
