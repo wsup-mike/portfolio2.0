23e93c47634c511eeb912fc92e46cfe44e22f7d3
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 15:32:36 -0500

    WorkExper-on <div> with ExperCards- more atts

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -16,7 +16,7 @@ function WorkExperience({}: Props) {
         <h3
             className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-2xl'
         >Work Experience</h3>
-        <div>
+        <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
             <ExperienceCard />
             <ExperienceCard />
             <ExperienceCard />
