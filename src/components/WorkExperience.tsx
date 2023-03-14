641bb9639f69941565b9516177d45a5944253138
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 14:06:25 -0500

    New h3 for title with classname atts

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -4,7 +4,11 @@ type Props = {}
 
 function WorkExperience({}: Props) {
   return (
-    <div className='h-screen'>WorkExperience</div>
+    <div className='h-screen'>
+        <h3
+            className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-2xl'
+        >Work Experience</h3>
+    </div>
   )
 }
 
