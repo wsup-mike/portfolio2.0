b274bd1bcb53386c47ef5969d1626bb265fb131d
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 14:21:50 -0500

    WorkExp-new <div> below h3 for ExperienceCards com

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -8,6 +8,12 @@ function WorkExperience({}: Props) {
         <h3
             className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-2xl'
         >Work Experience</h3>
+        <div>
+            {/* ExperienceCard */}
+            {/* ExperienceCard */}
+            {/* ExperienceCard */}
+            {/* ExperienceCard */}
+        </div>
     </div>
   )
 }
