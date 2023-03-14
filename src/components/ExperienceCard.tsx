f93e7830a237f31074ddfaef74fdc62666d8378b
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 14:39:39 -0500

    ExperienceCard-add atts to motion.div

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -5,7 +5,12 @@ type Props = {}
 
 function ExperienceCard({}: Props) {
   return (
-    <motion.div>Experience Card</motion.div>
+    <motion.div
+        className='h-screen flex relative overflow-hidden flex-col md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1}}
+        transition={{ duration: 1.0 }}
+    >Experience Card</motion.div>
   )
 }
 
