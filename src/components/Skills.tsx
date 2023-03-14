b7e7efe2a97944855b7a24c9af63bc3cf311c3c8
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 11:35:31 -0500

    Add <h3> with atts

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,7 +6,12 @@ type Props = {}
 
 function Skills({}: Props) {
   return (
-    <motion.div className='h-screen'>Skills</motion.div>
+    <motion.div className='flex relative flex-col text-center md:text-left lg:flex-row max-w-[2000px] lg:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
+        <h3
+            className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-2xl'
+        >Skills</h3>
+        <h3></h3>
+    </motion.div>
   )
 }
 
--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -11,7 +11,6 @@ function WorkExperience({}: Props) {
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1}}
         transition={{ duration: 1.0 }}
-    
     >
         <h3
             className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-2xl'
