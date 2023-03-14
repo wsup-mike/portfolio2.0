65bd23b6ecbe95dc836700b5d530e08f462ffb12
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 13:54:47 -0500

    Cr8 new component: WorkExperience.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,7 +9,7 @@ function About({}: Props) {
         className='h-screen flex-col relative flex text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
-        transition={{ duration: 1.0 }}
+        transition={{ duration: 2.0 }}
     >
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
         <motion.img 
