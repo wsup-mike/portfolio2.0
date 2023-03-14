82206068e2c38a91af1c4ec57d81489849a5c31f
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 12:40:27 -0500

    Apply filter group grayscale

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -11,7 +11,7 @@ function Skill({ directionLeft, directionRight }: Props) {
     <div className='group relative flex cursor-pointer'>
         <motion.img 
             src='/assets/ReactIcon.png'
-            className='rounded border border-gray-500 object-cover w-12 h-12 lg:w-24 lg:h-24'
+            className='rounded border border-gray-500 object-cover w-12 h-12 lg:w-24 lg:h-24 filter group-hover:grayscale'
             initial={{
                 x: directionLeft ? -200 : 200,
                 opacity: 0,
