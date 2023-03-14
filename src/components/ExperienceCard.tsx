baebf4f6abc8aa81595e9bb40ba97de5b190b258
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 14:47:51 -0500

    ExperienceCard-return <article> instead

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import { motion } from 'framer-motion'
 
 type Props = {}
 
 function ExperienceCard({}: Props) {
   return (
-    <motion.div
-        
-    >
-        <h3 className=''>Experience Card</h3>
-    </motion.div>
+    <article></article>
   )
 }
 
