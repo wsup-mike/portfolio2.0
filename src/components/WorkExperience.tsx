73a41dc4e51f1bada7cac3a60666671ef9e55180
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 14:25:01 -0500

    Create outer div as motion.div

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 function WorkExperience({}: Props) {
   return (
-    <div className='h-screen flex relative overflow-hidden flex-col md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
+    <motion.div className='h-screen flex relative overflow-hidden flex-col md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
         <h3
             className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-2xl'
         >Work Experience</h3>
@@ -15,7 +15,7 @@ function WorkExperience({}: Props) {
             {/* ExperienceCard */}
             {/* ExperienceCard */}
         </div>
-    </div>
+    </motion.div>
   )
 }
 
