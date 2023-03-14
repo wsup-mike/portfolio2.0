8aa1abad427c9334bcfb063bdcce1cb439fea73e
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-13 20:33:43 -0500

    Add h3 to 'About'

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,9 @@ type Props = {}
 
 function About({}: Props) {
   return (
-    <div className='h-screen flex-col relative flex text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>About</div> // now snap to center will snap this component 2 center
+    <div className='h-screen flex-col relative flex text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+        <h3>About</h3>
+    </div> // now snap to center will snap this component 2 center
   )
 }
 
