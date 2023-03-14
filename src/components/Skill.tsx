7583b42303d1b083997a023bc1a922026cdab4ac
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 12:57:09 -0500

    Next outer div, to create layer effect!

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -11,7 +11,7 @@ function Skill({ directionLeft, directionRight }: Props) {
     <div className='group relative flex cursor-pointer'>
         <motion.img 
             src='/assets/ReactIcon.png'
-            className='rounded border border-gray-500 object-cover w-12 h-12 lg:w-24 lg:h-24 filter group-hover:grayscale transition duration-700 ease-in-out'
+            className='rounded border border-gray-500 object-cover w-12 h-12 md:w-20 md:h-20 lg:w-24 lg:h-24 filter group-hover:grayscale transition duration-700 ease-in-out'
             initial={{
                 x: directionLeft ? -200 : 200,
                 opacity: 0,
@@ -22,7 +22,7 @@ function Skill({ directionLeft, directionRight }: Props) {
                 x: 0,
             }}
         />
-        <div>
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-700 ease-in-out group-hover:bg-white h-12 w-12 md:w-20 md:h-20 lg:w-24 lg:h-24 rounded z-0'>
             <div className='flex items-center justify-center h-full'>
                 <p className='text-3xl font-bold text-black opacity-100'>10</p>
             </div>
