14838a51b614bc48687c8fa68301b9193a8df875
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-14 12:13:15 -0500

    2 motion.div: initial,whieInView,transition

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,13 +7,34 @@ type Props = {}
 
 function Skills({}: Props) {
   return (
-    <motion.div className='flex relative flex-col text-center md:text-left lg:flex-row max-w-[2000px] lg:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
+    <motion.div 
+        className='flex relative flex-col text-center md:text-left lg:flex-row max-w-[2000px] lg:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+
+    >
         <h3
             className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-2xl'
         >Skills</h3>
         <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>Hover for more info</h3>
 
         <div className='grid grid-cols-4 gap-5'>
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
             <Skill />
         </div>
     </motion.div>
