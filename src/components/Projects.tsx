e6de897129159a6bfef8c2fbfd10edd5dd487256
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-15 14:56:16 -0500

    Style img

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -12,6 +12,7 @@ function Projects({}: Props) {
             {/* Projects here */}
             {projects.map((project, i) => (
                 <div className='w-screen  h-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44'>
+                    <img className='w-40 h-25' src='/assets/phoneMockup.png' />
                     <p className='text-4xl font-semibold text-center'>This is project#:</p>
                     <p>{project}</p>
                     <span className=''>Case Study {i+1} of {projects.length}</span>
