4c269c6356721628e65f7d78ba915dcc850b369c
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-15 10:19:52 -0500

    More boilerplate atts on the <div>

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,8 +4,12 @@ type Props = {}
 
 function Projects({}: Props) {
   return (
-    <div className='h-screen relative'>
+    <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
         <h3 className='absolute top-24 uppercase text-gray-500 tracking-[20px] text-2xl'>Projects</h3>
+        
+        <div className=''>
+
+        </div>
     </div>
   )
 }
