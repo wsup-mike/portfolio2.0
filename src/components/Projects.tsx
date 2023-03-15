69bacce242d9669ff745715d3782270c1db76f13
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-15 09:51:51 -0500

    Add h3 with styling atts

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,7 +4,9 @@ type Props = {}
 
 function Projects({}: Props) {
   return (
-    <div className='h-screen'>Projects</div>
+    <div className='h-screen'>
+        <h3 className='absolute top-24 uppercase text-gray-500 tracking-[20px] text-2xl'>Projects</h3>
+    </div>
   )
 }
 
