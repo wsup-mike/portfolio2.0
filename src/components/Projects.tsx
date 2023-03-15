efe8e66831c3ccc1d53030adf2fc41db9b1d6c80
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-15 10:27:34 -0500

    Add skewed background banner line with <div>

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,7 +7,7 @@ function Projects({}: Props) {
     <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
         <h3 className='absolute top-24 uppercase text-gray-500 tracking-[20px] text-2xl'>Projects</h3>
         
-        <div className=''>
+        <div className='w-full absolute top-[30%] bg-[#0e2433] left-0 h-[250px] -skew-y-12'>
 
         </div>
     </div>
