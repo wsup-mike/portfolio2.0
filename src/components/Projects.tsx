2db985277ccd321e8df06a85b026d70ae98e7daa
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-15 14:08:18 -0500

    Add atts to div

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,7 +7,8 @@ function Projects({}: Props) {
     <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
         <h3 className='absolute top-24 uppercase text-gray-500 tracking-[20px] text-2xl'>Projects</h3>
 
-        <div>
+        <div className='relative w-full flex overflow-x-scroll snap-x snap-x-mandatory z-20'>
+            <p>Project 1</p>
             {/* Projects here */}
             {/* Projects here */}
             {/* Projects here */}
