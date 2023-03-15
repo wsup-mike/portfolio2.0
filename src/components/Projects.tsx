6fdd9df3ff8d5aa8f1f7ad095dbf5c7169d87010
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-15 14:10:52 -0500

    Projects-cr8 dummy 'project's array 2 sim projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 type Props = {}
 
 function Projects({}: Props) {
+    const projects = [1, 2, 3, 4, 5];
   return (
     <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
         <h3 className='absolute top-24 uppercase text-gray-500 tracking-[20px] text-2xl'>Projects</h3>
@@ -10,9 +11,7 @@ function Projects({}: Props) {
         <div className='relative w-full flex overflow-x-scroll snap-x snap-x-mandatory z-20'>
             <p>Project 1</p>
             {/* Projects here */}
-            {/* Projects here */}
-            {/* Projects here */}
-            {/* Projects here */}
+            
         </div>
         
         <div className='w-full absolute top-[30%] bg-[#0e2433] left-0 h-[250px] -skew-y-12'>
