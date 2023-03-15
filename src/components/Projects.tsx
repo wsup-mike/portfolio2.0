dc9706d22f3cd14cdcbdc46ce95c92f3b809b2ba
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-15 14:33:24 -0500

    style atts on mapped <div> projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,13 +8,13 @@ function Projects({}: Props) {
     <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
         <h3 className='absolute top-24 uppercase text-gray-500 tracking-[20px] text-2xl'>Projects</h3>
 
-        <div className='relative w-full flex overflow-x-scroll snap-x snap-x-mandatory z-20'>
+        <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
             {/* Projects here */}
-            {projects.map((project) => (
-                <div>
+            {projects.map((project, i) => (
+                <div className='w-screen  h-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44'>
                     <p>This is project#:</p>
                     <p>{project}</p>
-
+                    <span>Case Study {i+1} of {projects.length}</span>
                 </div>
             ))}
         </div>
