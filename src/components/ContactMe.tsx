b3317110d3e8dde6cbdcfa16a952d13a8e9c868c
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-15 15:32:24 -0500

    ContactMe-Add new <div>

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -6,8 +6,10 @@ function ContactMe({}: Props) {
   return (
     <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
         <h3 className='absolute top-24 uppercase text-gray-500 tracking-[20px] text-2xl'>Contact Me</h3>
-    
-        <div className='h-screen'>ContactMe</div>
+        
+        <div>
+            <h4>Need to tweak your Apple or Android mobile app! Call now!</h4>
+        </div>
     </div>
   )
 }
