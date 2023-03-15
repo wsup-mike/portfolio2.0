00ac5465dbab926b7db0613429ffe7cda68b8dc7
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-15 09:37:30 -0500

    index-import n insert <Projects />

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Hero from '@/components/Hero';
 import About from '@/components/About';
 import WorkExperience from '@/components/WorkExperience';
 import Skills from '@/components/Skills';
+import Projects from '@/components/Projects';
 
 export default function Home() {
   return (
@@ -38,7 +39,7 @@ export default function Home() {
 
     {/* Projects */}
     <section id="projects" className='snap-center'>
-    
+      <Projects />
     </section>
 
     {/* Contact Me */}
