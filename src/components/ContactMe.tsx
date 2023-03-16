558164d6143ef01354a9f10372bcf44d0a7f4515
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-16 12:13:28 -0500

    Destruct handleSubmit, register fr useForm

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -14,6 +14,9 @@ type Inputs = {
 };
 
 function ContactMe({}: Props) {
+
+  const { register, handleSubmit } = useForm<Inputs>();
+  
   return (
     <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center'>
         <h3 className='absolute top-24 uppercase text-gray-500 tracking-[20px] text-2xl'>Contact Me</h3>
