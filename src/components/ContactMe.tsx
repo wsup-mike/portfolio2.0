b3bb10b9c6aeb2aa98f39a83acb17860099cbe3b
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-16 13:35:34 -0500

    On h4- more atts

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -4,11 +4,11 @@ type Props = {}
 
 function ContactMe({}: Props) {
   return (
-    <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
+    <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center'>
         <h3 className='absolute top-24 uppercase text-gray-500 tracking-[20px] text-2xl'>Contact Me</h3>
         
         <div>
-            <h4>
+            <h4 className='text-4xl font-semibold text-center'>
               Need an Apple or Android mobile app! <span className='decoration-[#f7ab0a]/30 underline'>Send me a message!</span>
             </h4>
         </div>
