59d302b8b42333c7aea3ae3447b426793b5bbf5e
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-16 13:36:42 -0500

    outer <div>- apply flex n more

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -6,8 +6,8 @@ function ContactMe({}: Props) {
   return (
     <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center'>
         <h3 className='absolute top-24 uppercase text-gray-500 tracking-[20px] text-2xl'>Contact Me</h3>
-        
-        <div>
+  
+        <div className='flex flex-col space-y-10'>
             <h4 className='text-4xl font-semibold text-center'>
               Need an Apple or Android mobile app! <span className='decoration-[#f7ab0a]/30 underline'>Send me a message!</span>
             </h4>
