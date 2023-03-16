455063b37cbfb555e124e09e482d9cfa9f42a615
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-16 14:08:51 -0500

    Header-Add Link to email icon to 'contact'

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,33 +31,35 @@ function Header({}: Props) {
             <SocialIcon url='https://www.youtube.com/sonnysangha' className='border border-white transform scale-75 md:scale-100 lg:scale-125' fgColor='gray' bgColor='transparent'/>
             <SocialIcon url='https://www.youtube.com/sonnysangha' className='border border-white transform scale-75 md:scale-100 lg:scale-125' fgColor='gray' bgColor='transparent'/>
         </motion.div>
-        <motion.div 
-            className='flex flex-row items-center text-gray-300 cursor-pointer'
-            initial={{ // the animation's starting point
-                x: 500, //start 500 pixels from right
-                opacity: 0, // starts completely invisible
-                scale: 0.5, // starts at half of original size
-            }}
-            animate={{
-                x: 0, // once fully animated, ends at initial starting point
-                opacity: 1, // becomes 100% opaque
-                scale: 1, // becomes fully enlarged to full size
-            }}
-            transition={{
-                duration: 1.0,
-            }}
-        >
-            {/* <Link href="#contactme"> */}
-                <SocialIcon 
-                    className='cursor-pointer border border-white transform scale-75 md:scale-100 lg:scale-125'
-                    network='email'
-                    fgColor='gray'
-                    bgColor='transparent'
-                    href='#contactme'
-                />
-                <p className='uppercase hidden md:inline-flex text-xs text-gray-500'>Get in touch</p>
-            {/* </Link> */}
-        </motion.div>
+
+        <Link href='#contact'>
+            <motion.div 
+                className='flex flex-row items-center text-gray-300 cursor-pointer'
+                initial={{ // the animation's starting point
+                    x: 500, //start 500 pixels from right
+                    opacity: 0, // starts completely invisible
+                    scale: 0.5, // starts at half of original size
+                }}
+                animate={{
+                    x: 0, // once fully animated, ends at initial starting point
+                    opacity: 1, // becomes 100% opaque
+                    scale: 1, // becomes fully enlarged to full size
+                }}
+                transition={{
+                    duration: 1.0,
+                }}
+            >
+                {/* <Link href="#contactme"> */}
+                    <SocialIcon 
+                        className='cursor-pointer border border-white transform scale-75 md:scale-100 lg:scale-125'
+                        network='email'
+                        fgColor='gray'
+                        bgColor='transparent'
+                    />
+                    <p className='uppercase hidden md:inline-flex text-xs text-gray-500'>Get in touch</p>
+                {/* </Link> */}
+            </motion.div>
+        </Link>
     </header>
 
   )
