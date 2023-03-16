26b61dbf399b1a7e463e355469ebde083d195bad
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-16 13:59:54 -0500

    PhoneIcon set

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -9,11 +9,14 @@ function ContactMe({}: Props) {
         <h3 className='absolute top-24 uppercase text-gray-500 tracking-[20px] text-2xl'>Contact Me</h3>
   
         <div className='flex flex-col space-y-10'>
-            <h4 className='text-4xl font-semibold text-center'>
+            <h4 className='text-xl font-semibold text-center'>
               Need an Apple or Android mobile app! <span className='decoration-[#f7ab0a]/30 underline'>Send me a message!</span>
             </h4>
             <div>
-
+              <div className='flex items-center space-x-5 justify-center'>
+                <PhoneIcon className='text-[#f7ab0a] h-5 w-5 animate-pulse'/>
+                <p>+1 773 459 3603</p>
+              </div>
             </div>
         </div>
     </div>
