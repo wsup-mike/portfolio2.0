6d4eff0b4e12400f35c8a98d9e70ae6ae01a2f30
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-20 11:19:29 -0500

    box 1 flex

--- src/components/CSSPractice.tsx
+++ src/components/CSSPractice.tsx
@@ -6,13 +6,13 @@ function CSSPractice({}: Props) {
   return (
     <div className='relative h-screen bg-purple-800 flex justify-center items-center flex-col'>
         <p>CSS Practice</p>
-        <div className='absolute bg-blue-500 h-[400px] w-[400px]'>
+        <div className='absolute flex bg-blue-500 h-[400px] w-[400px]'>
             <p>Box 1</p>
-            <div className='bg-orange-700 h-[350px] w-[350px]'>
+            <div className='absolute bg-orange-700 h-[350px] w-[350px]'>
                 <p>Box 2</p>
-                <div className='bg-green-800 h-[300px] w-[300px]'>
+                <div className='absolute bg-green-800 h-[300px] w-[300px]'>
                     <p>Box 3</p>
-                    <div className='bg-red-800 h-[250px] w-[250px]'>
+                    <div className='absolute bg-red-800 h-[250px] w-[250px]'>
                         <p>Box 4</p>
                     </div>
                 </div>
