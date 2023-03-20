e465abc687186f0e88b6084b0d8508cfa65c55de
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-20 11:43:39 -0500

    Add z-10 to CSS Practice

--- src/components/CSSPractice.tsx
+++ src/components/CSSPractice.tsx
@@ -5,7 +5,7 @@ type Props = {}
 function CSSPractice({}: Props) {
   return (
     <div className='relative h-screen bg-purple-800 flex justify-center items-center flex-col'>
-        <p>CSS Practice</p>
+        <p className='z-10'>CSS Practice</p>
         <div className='absolute bg-blue-500 h-[400px] w-[400px] flex justify-center items-center'>
             <p>Box 1</p>
             <div className='absolute bg-orange-700 h-[350px] w-[350px] flex justify-center items-center'>
