4ff58742e5202de3e21158d969502aceca6f0c19
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-20 11:45:14 -0500

    Another z-10

--- src/components/CSSPractice.tsx
+++ src/components/CSSPractice.tsx
@@ -7,9 +7,9 @@ function CSSPractice({}: Props) {
     <div className='relative h-screen bg-purple-800 flex justify-center items-center flex-col'>
         <p className='z-10'>CSS Practice</p>
         <div className='absolute bg-blue-500 h-[400px] w-[400px] flex justify-center items-center'>
-            <p>Box 1</p>
+            <p className='z-10'>Box 1</p>
             <div className='absolute bg-orange-700 h-[350px] w-[350px] flex justify-center items-center'>
-                <p>Box 2</p>
+                <p  className='z-10'>Box 2</p>
                 <div className='absolute bg-green-800 h-[300px] w-[300px] flex justify-center items-center'>
                     <p>Box 3</p>
                     <div className='absolute bg-red-800 h-[250px] w-[250px]'>
