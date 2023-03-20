3365ae0bf5d2a8a64a5463276b0b42d18eeab548
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-20 12:07:54 -0500

    all text-xs

--- src/components/CSSPractice.tsx
+++ src/components/CSSPractice.tsx
@@ -7,15 +7,15 @@ function CSSPractice({}: Props) {
     <div className=' h-screen bg-purple-800 flex justify-center items-center flex-col'>
         <p>CSS Practice</p>
         <div className=' bg-blue-500 h-[400px] w-[400px] flex justify-center items-center flex-col'>
-            <p>Box 1</p>
+            <p className='text-xs'>Box 1</p>
             <div className=' bg-orange-700 h-[350px] w-[350px] flex justify-center items-center'>
-                <p>Box 2</p>
+                <p className='text-xs'>Box 2</p>
                 <div className=' bg-green-800 h-[300px] w-[300px] flex justify-center items-center'>
-                    <p>Box 3</p>
+                    <p className='text-xs'>Box 3</p>
                     <div className=' bg-red-800 h-[250px] w-[250px] flex justify-center items-center'>
-                        <p>Box 4</p>
+                        <p className='text-xs'>Box 4</p>
                         <div className=' bg-yellow-600 h-[100px] w-[200px]'>
-                            <p>Box 5</p>
+                            <p className='text-xs'>Box 5</p>
                         </div>
                     </div>
                 </div>
