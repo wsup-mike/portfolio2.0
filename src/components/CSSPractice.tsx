2ca64eae7bc50237da35ff5a3565d1327884daa3
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-20 11:56:03 -0500

    Remove all absolute positions

--- src/components/CSSPractice.tsx
+++ src/components/CSSPractice.tsx
@@ -5,15 +5,18 @@ type Props = {}
 function CSSPractice({}: Props) {
   return (
     <div className='relative h-screen bg-purple-800 flex justify-center items-center flex-col'>
-        <p className='z-10'>CSS Practice</p>
-        <div className='absolute bg-blue-500 h-[400px] w-[400px] flex justify-center items-center'>
-            <p className='z-10'>Box 1</p>
-            <div className='absolute bg-orange-700 h-[350px] w-[350px] flex justify-center items-center'>
-                <p  className='z-10'>Box 2</p>
-                <div className='absolute bg-green-800 h-[300px] w-[300px] flex justify-center items-center'>
+        <p>CSS Practice</p>
+        <div className='relative bg-blue-500 h-[400px] w-[400px] flex justify-center items-center'>
+            <p>Box 1</p>
+            <div className='relative bg-orange-700 h-[350px] w-[350px] flex justify-center items-center'>
+                <p>Box 2</p>
+                <div className='relative bg-green-800 h-[300px] w-[300px] flex justify-center items-center'>
                     <p>Box 3</p>
-                    <div className='absolute bg-red-800 h-[250px] w-[250px]'>
+                    <div className='relative bg-red-800 h-[250px] w-[250px] flex justify-center items-center'>
                         <p>Box 4</p>
+                        <div className='relative bg-yellow-600 h-[100px] w-[200px]'>
+                            <p>Box 5</p>
+                        </div>
                     </div>
                 </div>
             </div>
