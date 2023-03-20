3768b1aaa10cd7db1f4210efea3927378a7f1d30
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-20 10:45:41 -0500

    Make Box1 text absolute

--- src/components/CSSPractice.tsx
+++ src/components/CSSPractice.tsx
@@ -6,8 +6,8 @@ function CSSPractice({}: Props) {
   return (
     <div className='relative h-screen pt-20 bg-purple-500 flex justify-center items-center flex-col'>
         <p>CSS Practice</p>
-        <div className='relative bg-red-900 h-80 w-80 flex justify-center items-center'>
-            <p className='text-white '>Box 1</p>
+        <div className='absolute bg-red-900 h-80 w-80 flex justify-center items-center top-0'>
+            <p className='text-white absolute left-0 top-0'>Box 1</p>
             <div className='bg-blue-500 h-70 w-70 flex justify-center items-center'>
                 <p className='text-white'>Box 2</p>
             </div>
