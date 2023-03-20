e7d87ace35b6b190bfb2253f7ad5374445c93511
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-20 10:00:18 -0500

    Practice commit

--- src/components/CSSPractice.tsx
+++ src/components/CSSPractice.tsx
@@ -6,7 +6,7 @@ function CSSPractice({}: Props) {
   return (
     <div className='h-screen pt-20 bg-purple-500'>
         <p>CSS Practice</p>
-        <div className='bg-red-900'>
+        <div className='bg-red-900 h-100 w-100'>
             <p className='text-white'>Box 1</p>
         </div>
     </div>
