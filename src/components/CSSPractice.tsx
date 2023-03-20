ebc94f9cc2a3e82328647f445162a438b70ee40d
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-20 10:33:45 -0500

    remove border

--- src/components/CSSPractice.tsx
+++ src/components/CSSPractice.tsx
@@ -4,9 +4,9 @@ type Props = {}
 
 function CSSPractice({}: Props) {
   return (
-    <div className='h-screen pt-20 bg-purple-500 flex justify-center items-center flex-col border border-yellow-400'>
+    <div className='h-screen pt-20 bg-purple-500 flex justify-center items-center flex-col'>
         <p>CSS Practice</p>
-        <div className='bg-red-900 h-80 w-80 flex justify-center items-center'>
+        <div className=' bg-red-900 h-80 w-80 flex justify-center items-center'>
             <p className='text-white '>Box 1</p>
             <div className='bg-blue-500 h-70 w-70 flex justify-center items-center'>
                 <p className='text-white'>Box 2</p>
