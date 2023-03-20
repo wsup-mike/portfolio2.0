435930bc380e6454fa40558c65a5bc1693fb86ef
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-20 11:42:16 -0500

    Add flex j-center i-center

--- src/components/CSSPractice.tsx
+++ src/components/CSSPractice.tsx
@@ -8,9 +8,9 @@ function CSSPractice({}: Props) {
         <p>CSS Practice</p>
         <div className='absolute bg-blue-500 h-[400px] w-[400px]'>
             <p>Box 1</p>
-            <div className='absolute bg-orange-700 h-[350px] w-[350px]'>
+            <div className='absolute bg-orange-700 h-[350px] w-[350px] flex justify-center items-center'>
                 <p>Box 2</p>
-                <div className='absolute bg-green-800 h-[300px] w-[300px]'>
+                <div className='absolute bg-green-800 h-[300px] w-[300px] flex justify-center items-center'>
                     <p>Box 3</p>
                     <div className='absolute bg-red-800 h-[250px] w-[250px]'>
                         <p>Box 4</p>
