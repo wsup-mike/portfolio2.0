a2767ab44a85a80c4c3ad1bb01c5a5a09b9049c2
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-20 10:55:46 -0500

    next commit

--- src/components/CSSPractice.tsx
+++ src/components/CSSPractice.tsx
@@ -4,9 +4,9 @@ type Props = {}
 
 function CSSPractice({}: Props) {
   return (
-    <div className=' h-screen pt-20 bg-purple-500 flex justify-center items-center flex-col'>
+    <div className='relative h-screen pt-20 bg-purple-500 flex justify-center items-center flex-col'>
         <p>CSS Practice</p>
-        <div className='absolute bg-red-900 h-80 w-80 flex justify-center items-center top-48'>
+        <div className='absolute bg-red-900 h-80 w-80 flex justify-center items-center top-60'>
             <p className='text-white absolute left-0 top-0'>Box 1</p>
             <div className='bg-blue-500  flex justify-center items-center'>
                 <p className='text-white'>Box 2</p>
