bdfde7a13144eaba195c22da6d597a73a29cac05
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-20 13:32:37 -0500

    Box 6 relative with coords

--- src/components/CSSPractice.tsx
+++ src/components/CSSPractice.tsx
@@ -10,15 +10,17 @@ function CSSPractice({}: Props) {
             <p className='text-xs border border-white'>Box 1</p>
             <div className=' bg-orange-700 h-[350px] w-[350px] flex justify-center items-center flex-col'>
                 <p className='text-xs'>Box 2</p>
-                <div className='relative top-10 left-10 bg-green-800 h-[300px] w-[300px] flex justify-center items-center flex-col'>
+                <div className=' bg-green-800 h-[300px] w-[300px] flex justify-center items-center flex-col'>
                     <p className='text-xs'>Box 3</p>
-                    <div className=' bg-red-800 h-[250px] w-[250px] flex justify-center items-center flex-row'>
-                        <p className='text-xs'>Box 4</p>
-                        <div className=' bg-yellow-600 h-[100px] w-[100px] flex justify-center items-center'>
-                            <p className='text-xs'>Box 5</p>
-                        </div>
-                        <div className='relative left-3 bg-blue-600 h-[100px] w-[100px] flex justify-center items-center'>
-                            <p className='text-xs'>Box 6</p>
+                    <div className=' bg-red-800 h-[250px] w-[250px] flex justify-center items-center flex-col'>
+                        <p className='text-xs'>Box 4</p>    
+                        <div className='flex flex-row'>
+                            <div className=' bg-yellow-600 h-[100px] w-[100px] flex justify-center items-center'>
+                                <p className='text-xs'>Box 5</p>
+                            </div>
+                            <div className='relative top-10 left-10 bg-blue-600 h-[100px] w-[100px] flex justify-center items-center'>
+                                <p className='text-xs'>Box 6</p>
+                            </div>
                         </div>
                     </div>
                 </div>
