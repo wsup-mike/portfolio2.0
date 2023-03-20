dd4091e5c9130f6c89dd4a256b9837f6564ae6b5
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-20 11:17:06 -0500

    make box 1 absolute

--- src/components/CSSPractice.tsx
+++ src/components/CSSPractice.tsx
@@ -6,13 +6,13 @@ function CSSPractice({}: Props) {
   return (
     <div className='h-screen bg-purple-800 flex justify-center items-center flex-col'>
         <p>CSS Practice</p>
-        <div className='bg-blue-500 h-[400px] w-[400px]'>
+        <div className='absolute bg-blue-500 h-[400px] w-[400px]'>
             <p>Box 1</p>
             <div className='bg-orange-700 h-[350px] w-[350px]'>
                 <p>Box 2</p>
                 <div className='bg-green-800 h-[300px] w-[300px]'>
                     <p>Box 3</p>
-                    <div className='bg-red-800'>
+                    <div className='bg-red-800 h-[250px] w-[250px]'>
                         <p>Box 4</p>
                     </div>
                 </div>
