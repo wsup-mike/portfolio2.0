a2e706059353e6efd04388e0f71b4bfc1eea66e0
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-20 10:26:36 -0500

    New edits

--- src/components/CSSPractice.tsx
+++ src/components/CSSPractice.tsx
@@ -7,7 +7,7 @@ function CSSPractice({}: Props) {
     <div className='h-screen pt-20 bg-purple-500 flex justify-center items-center flex-col border border-yellow-400'>
         <p>CSS Practice</p>
         <div className='bg-red-900 h-80 w-80 flex justify-center items-center'>
-            <p className='text-white self-start place-self-start'>Box 1</p>
+            <p className='text-white self-start '>Box 1</p>
             <div className='bg-blue-500 h-70 w-70 flex justify-center items-center'>
                 <p className='text-white'>Box 2</p>
             </div>
