beca4b28167adb563ab3518c517e9bcd41d29315
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-20 11:04:26 -0500

    outer parent bg purple

--- src/components/CSSPractice.tsx
+++ src/components/CSSPractice.tsx
@@ -4,8 +4,17 @@ type Props = {}
 
 function CSSPractice({}: Props) {
   return (
-    <div className='h-screen'>
+    <div className='h-screen bg-purple-800'>
         <p>CSS Practice</p>
+        <div>
+            <p>Box 1</p>
+            <div>
+                <p>Box 2</p>
+                <div>
+                    <p>Box 3</p>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
