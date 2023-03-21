e0055a062b6913b97ddf851a35f43a8f84b7f44b
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-21 11:21:25 -0500

    margin top

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -7,7 +7,7 @@ type Props = {}
 function WorkExperience({}: Props) {
   return (
     <motion.div 
-        className='h-screen flex relative overflow-hidden flex-col md:flex-row max-w-full px-6 md:px-10 justify-center mx-auto items-center '
+        className='h-screen flex relative overflow-hidden flex-col md:flex-row max-w-full px-6 md:px-10 justify-center mx-auto items-center'
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1}}
         transition={{ duration: 1.0 }}
@@ -15,7 +15,7 @@ function WorkExperience({}: Props) {
         <h3
             className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-xs sm:text-2xl'
         >Work Experience</h3>
-        <div className='w-full sm:space-x-9 flex overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-pink-400 scrollbar-thumb-teal-600 sm:mt-72 mt-24'>
+        <div className='w-full sm:space-x-9 flex overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-pink-400 scrollbar-thumb-teal-600 mt-72'>
             <ExperienceCard />
             <ExperienceCard />
             <ExperienceCard />
