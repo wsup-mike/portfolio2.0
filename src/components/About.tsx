910218de31b019ea99187dc48bd11a03aeb21787
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-21 09:15:37 -0500

    Make h3 absolute, not relative

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,14 +5,13 @@ type Props = {}
 
 function About({}: Props) {
   return (
-    
         <motion.div 
             className=' h-screen flex-col relative flex text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 2.0 }}
         >
-            <h3 className='h-screen flex relative top-20 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
+            <h3 className='absolute h-screen flex top-20 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
 
             <motion.img 
                 src='./assets/cortez.JPG'
