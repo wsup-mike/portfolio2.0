b6c4f00fa68d2483a17f908fa91631e1db24d268
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-21 10:24:44 -0500

    WorkExperience-remove on h3 size of text

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -7,13 +7,13 @@ type Props = {}
 function WorkExperience({}: Props) {
   return (
     <motion.div 
-        className='h-screen flex relative overflow-hidden flex-col md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'
+        className='h-screen flex relative overflow-hidden flex-col md:flex-row max-w-full px-10 justify-center mx-auto items-center'
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1}}
         transition={{ duration: 1.0 }}
     >
         <h3
-            className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-2xl'
+            className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-xs sm:text-2xl'
         >Work Experience</h3>
         <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-pink-400 scrollbar-thumb-teal-600'>
             <ExperienceCard />
