3263ad336d6e32d2442def0acd31ddac5d6aee96
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-20 19:11:48 -0500

    Header-replace SocialIcons with hero icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { SocialIcon } from 'react-social-icons'
 import { motion } from 'framer-motion'
 import Link from 'next/link'
+import { EnvelopeIcon } from '@heroicons/react/24/solid' 
 
 type Props = {}
 
@@ -9,7 +10,7 @@ function Header({}: Props) {
   return (
     <header className='sticky top-0 p-2 flex items-start justify-between max-w-6xl mx-auto z-20'>
         <motion.div 
-            className='flex flex-row items-center space-x-0'
+            className='flex flex-row items-center space-x-2 md:space-x-4'
             initial={{ // the animation's starting point
                 x: -500, //start 500 pixels from left
                 opacity: 0, // starts completely invisible
@@ -25,11 +26,18 @@ function Header({}: Props) {
             }}
         >
             {/* Social Icons */}
-            <SocialIcon url='https://www.youtube.com/sonnysangha' className='border border-white scale-50 sm:scale-100 ' fgColor='gray' bgColor='transparent' />
+            <EnvelopeIcon className='h-6 md:h-8 w-6 md:w-8 text-gray-500 border border-white'/>
+            <EnvelopeIcon className='h-6 md:h-8 w-6 md:w-8 text-gray-500 border border-white'/>
+            <EnvelopeIcon className='h-6 md:h-8 w-6 md:w-8 text-gray-500 border border-white'/>
+            <EnvelopeIcon className='h-6 md:h-8 w-6 md:w-8 text-gray-500 border border-white'/>
+            <EnvelopeIcon className='h-6 md:h-8 w-6 md:w-8 text-gray-500 border border-white'/>
+
+
+            {/* <SocialIcon url='https://www.youtube.com/sonnysangha' className='border border-white scale-50 sm:scale-100 ' fgColor='gray' bgColor='transparent' />
             <SocialIcon url='https://www.youtube.com/sonnysangha' className='border border-white  scale-50 sm:scale-100' fgColor='gray' bgColor='transparent'/>
             <SocialIcon url='https://www.youtube.com/sonnysangha' className='border border-white  scale-50 sm:scale-100' fgColor='gray' bgColor='transparent'/>
             <SocialIcon url='https://www.youtube.com/sonnysangha' className='border border-white  scale-50 sm:scale-100' fgColor='gray' bgColor='transparent'/>
-            <SocialIcon url='https://www.youtube.com/sonnysangha' className='border border-white  scale-50 sm:scale-100' fgColor='gray' bgColor='transparent'/>
+            <SocialIcon url='https://www.youtube.com/sonnysangha' className='border border-white  scale-50 sm:scale-100' fgColor='gray' bgColor='transparent'/> */}
         </motion.div>
 
         <Link href='#contact'>
@@ -50,12 +58,13 @@ function Header({}: Props) {
                 }}
             >
                 {/* <Link href="#contactme"> */}
-                    <SocialIcon 
+                <EnvelopeIcon className='h-6 md:h-8 w-6 md:w-8 text-gray-500 border border-white'/>
+                    {/* <SocialIcon 
                         className='cursor-pointer border border-white  scale-50 sm:scale-100'
                         network='email'
                         fgColor='gray'
                         bgColor='transparent'
-                    />
+                    /> */}
                     <p className='uppercase hidden md:inline-flex text-xs text-gray-500'>Get in touch</p>
                 {/* </Link> */}
             </motion.div>
