130280992ebb2c8f19ef971afba95e5c16e295e1
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-21 09:42:24 -0500

    text-xs vs text-base

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,12 +6,12 @@ type Props = {}
 function About({}: Props) {
   return (
         <motion.div 
-            className=' h-screen flex-col relative flex text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'
+            className=' h-screen flex-col relative flex text-center md:text-left md:flex-row max-w-7xl px-10 justify-around mx-auto items-center'
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 2.0 }}
         >
-            <h3 className='relative flex top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
+            <h3 className='absolute flex top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
 
             <motion.img 
                 src='./assets/cortez.JPG'
@@ -32,7 +32,7 @@ function About({}: Props) {
             />
             <div className='space-y-2 px-0 md:px-10'>
                 <h4 className='sm:text-md md:text-xl lg:text-4xl font-semibold'>Here is a <span className='underline decoration-[#f7ab0a]/50'>little</span> background info</h4>
-                <p className='text-base'>
+                <p className='text-base sm:text-xs'>
                     Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage prosciutto chicken spare ribs salami picanha doner. Kevin capicola sausage, buffalo bresaola venison turkey shoulder picanha ham pork tri-tip meatball meatloaf ribeye. Doner spare ribs andouille bacon sausage. Ground round jerky brisket pastrami shank.
                 </p>
             </div>
