6fff85b4e2c5e4823d0e620e8cf479dfbf9b8fdb
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-21 23:25:16 -0500

    Add lg:mt70

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -7,7 +7,7 @@ type Props = {}
 function WorkExperience({}: Props) {
   return (
     <motion.div 
-        className='h-screen flex relative overflow-hidden flex-col md:flex-row max-w-full px-6 md:px-10 justify-center mx-auto items-center mt-44'
+        className='h-screen flex relative overflow-hidden flex-col md:flex-row max-w-full px-6 md:px-10 justify-center mx-auto items-center '
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1}}
         transition={{ duration: 1.0 }}
@@ -16,7 +16,7 @@ function WorkExperience({}: Props) {
             // className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-md sm:text-2xl'
             className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-xl'
         >Work Experience</h3>
-        <div className='w-full sm:space-x-9 flex overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-pink-400 scrollbar-thumb-teal-600 mt-36'>
+        <div className='w-full sm:space-x-9 flex overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-pink-400 scrollbar-thumb-teal-600 mt-36 lg:mt-72'>
             <ExperienceCard />
             <ExperienceCard />
             <ExperienceCard />
