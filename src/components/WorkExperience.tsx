eb347ca3ac20d6af8ec4753a80291d0dde8b2e59
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-21 22:48:17 -0500

    text 2xl

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -13,9 +13,10 @@ function WorkExperience({}: Props) {
         transition={{ duration: 1.0 }}
     >
         <h3
-            className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-md sm:text-2xl'
+            // className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-md sm:text-2xl'
+            className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-xl'
         >Work Experience</h3>
-        <div className='w-full sm:space-x-9 flex overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-pink-400 scrollbar-thumb-teal-600 mt-32 md:mt-44'>
+        <div className='w-full sm:space-x-9 flex overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-pink-400 scrollbar-thumb-teal-600 mt-32'>
             <ExperienceCard />
             <ExperienceCard />
             <ExperienceCard />
