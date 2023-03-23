cb51ec8b12ab1a8e0d0cb37c5820f4ab46d7a28e
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-23 08:09:26 -0500

    mt-44 on parent at md

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -7,7 +7,7 @@ type Props = {}
 function WorkExperience({}: Props) {
   return (
     <motion.div 
-        className='h-screen flex mx-auto relative overflow-hidden flex-col md:flex-row max-w-full px-5 md:px-40 justify-start  items-center '
+        className='h-screen flex mx-auto relative overflow-hidden flex-col md:flex-row max-w-full px-5 md:px-40 justify-start items-center '
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1}}
         transition={{ duration: 1.0 }}
@@ -17,7 +17,7 @@ function WorkExperience({}: Props) {
             // className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-md sm:text-2xl'
             className='absolute top-24 uppercase tracking-[20px] text-center text-gray-500 text-sm sm:text-xl'
         >Work Experience</h3>
-        <div className='w-full space-x-5 flex overflow-x-scroll py-5 px-10 snap-x snap-mandatory scrollbar scrollbar-track-pink-400 scrollbar-thumb-teal-600 mt-36 md:mt-117 lg:mt-80'>
+        <div className='w-full space-x-5 flex overflow-x-scroll py-5 px-10 snap-x snap-mandatory scrollbar scrollbar-track-pink-400 scrollbar-thumb-teal-600 mt-36 md:mt-44 lg:mt-80'>
             <ExperienceCard />
             <ExperienceCard />
             <ExperienceCard />
