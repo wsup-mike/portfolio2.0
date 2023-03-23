15ab3dbb827eea59246d86cc868f5e491083bf95
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-23 14:28:55 -0500

    Skills-mt20 @ sm on grid icons

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -18,7 +18,7 @@ function Skills({}: Props) {
         >Skills</h3>
         <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>Hover for more info</h3>
 
-        <div className='grid grid-cols-4 gap-5 md:mt-32'>
+        <div className='grid grid-cols-4 gap-5 sm:mt-20 md:mt-32'>
             <Skill />
             <Skill />
             <Skill />
