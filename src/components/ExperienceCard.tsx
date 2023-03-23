8df2017797573f379d3bc4bcf45f78b96bdc0b63
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-22 21:56:27 -0500

    w-260

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 function ExperienceCard({}: Props) {
   return (
-    <article className='flex flex-col rounded-lg items-center space-y-1 flex-shrink-0 w-[245px] snap-center bg-[#292929] p-2 opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-500 overflow-hidden border border-yellow-400'>
+    <article className='flex flex-col rounded-lg items-center space-y-1 flex-shrink-0 w-[260px] snap-center bg-[#292929] p-2 opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-500 overflow-hidden border border-yellow-400'>
         <motion.img 
             className='w-10 h-10 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
             src='/assets/peanut.jpg'
