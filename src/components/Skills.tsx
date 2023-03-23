3010f9aae8793bfdad657ea44f09a397328ab31a
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-23 13:36:48 -0500

    Skills-directionLeft false

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -20,6 +20,7 @@ function Skills({}: Props) {
         <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>Hover for more info</h3>
 
         <div className='grid grid-cols-4 gap-5'>
+            <Skill directionLeft={false} />
             <Skill />
             <Skill />
             <Skill />
@@ -34,8 +35,7 @@ function Skills({}: Props) {
             <Skill />
             <Skill />
             <Skill />
-            <Skill />
-            <Skill />
+            {/* <Skill /> */}
         </div>
     </motion.div>
   )
