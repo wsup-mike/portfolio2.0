3b3f89b33112c9b4c7a8bf537b81b661153774fe
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-23 14:21:48 -0500

    Update to motion.img

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -9,7 +9,12 @@ type Props = {
 function Skill({ directionLeft, directionRight }: Props) {
   return (
     <div className='group relative flex cursor-pointer'>
-        <motion.img 
+        <img 
+            src='/assets/ReactIcon.png'
+            className='rounded border border-gray-500 object-cover w-12 h-12 md:w-20 md:h-20 lg:w-24 lg:h-24 filter group-hover:grayscale transition duration-700 ease-in-out'
+
+        />
+        {/* <motion.img 
             src='/assets/ReactIcon.png'
             className='rounded border border-gray-500 object-cover w-12 h-12 md:w-20 md:h-20 lg:w-24 lg:h-24 filter group-hover:grayscale transition duration-700 ease-in-out'
             initial={{
@@ -21,7 +26,7 @@ function Skill({ directionLeft, directionRight }: Props) {
                 opacity: 1,
                 x: 0,
             }}
-        />
+        /> */}
         <div className='absolute opacity-0 group-hover:opacity-80 transition duration-700 ease-in-out group-hover:bg-white h-12 w-12 md:w-20 md:h-20 lg:w-24 lg:h-24 rounded z-0'>
             <div className='flex items-center justify-center h-full'>
                 <p className='text-3xl font-bold text-black opacity-100'>10</p>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -20,7 +20,6 @@ function Skills({}: Props) {
         <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>Hover for more info</h3>
 
         <div className='grid grid-cols-4 gap-5'>
-            <Skill directionLeft={false} />
             <Skill />
             <Skill />
             <Skill />
