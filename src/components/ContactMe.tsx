a7ae1aafea9b467c56ea1709f136fa4246252e7b
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-23 21:05:33 -0500

    add border around 2nd div

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -32,7 +32,7 @@ function ContactMe({}: Props) {
     <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center '>
         <h3 className='absolute top-24 uppercase text-gray-500 tracking-[20px] text-center text-sm'>Contact Me</h3>
   
-        <div className='flex flex-col space-y-10 mt-56  w-full'>
+        <div className='flex flex-col space-y-10 mt-56 border border-yellow-300 w-full p-6'>
             <h4 className='text-sm font-semibold text-center whitespace-normal'>
               Need an Apple or Android mobile app! <span className='decoration-[#f7ab0a]/30 underline'>Send me a message!</span>
             </h4>
