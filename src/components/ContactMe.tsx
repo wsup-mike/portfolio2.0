8993ca6214bd06af0b6ad6c85f49b1b8dfab1243
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-23 20:37:21 -0500

    on h3, text-center

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -29,10 +29,10 @@ function ContactMe({}: Props) {
   
 
   return (
-    <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center mt-36'>
-        <h3 className='absolute top-24 uppercase text-gray-500 tracking-[20px] text-2xl'>Contact Me</h3>
+    <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center '>
+        <h3 className='absolute top-24 uppercase text-gray-500 tracking-[20px] text-center text-2xl'>Contact Me</h3>
   
-        <div className='flex flex-col space-y-10'>
+        <div className='flex flex-col space-y-10 mt-52'>
             <h4 className='text-xl font-semibold text-center'>
               Need an Apple or Android mobile app! <span className='decoration-[#f7ab0a]/30 underline'>Send me a message!</span>
             </h4>
