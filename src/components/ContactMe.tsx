73693bed152f011d030087c4c8009d8a1ad64942
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-23 20:32:57 -0500

    ContactMe-add mt36 top div

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -29,7 +29,7 @@ function ContactMe({}: Props) {
   
 
   return (
-    <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center'>
+    <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center mt-36'>
         <h3 className='absolute top-24 uppercase text-gray-500 tracking-[20px] text-2xl'>Contact Me</h3>
   
         <div className='flex flex-col space-y-10'>
